ee1033bebf1388822beb9ec2c61bd6a0c6dd13f2
QuocToancb <quoctoancb@gmail.com>
2020-08-28 14:25:52 +0700

    some commit

--- app/controllers/HomeController.scala
+++ app/controllers/HomeController.scala
@@ -17,8 +17,10 @@ class HomeController @Inject()(cc: ControllerComponents) extends AbstractControl
   def getData() = Action { implicit request: Request[AnyContent] => {
     val response = Json.toJson(UserData.findAll())
     val localhost: InetAddress = InetAddress.getLocalHost
+	
+	
     val localIpAddress: String = localhost.getHostAddress
-
+             
     println(s"localIpAddress = $localIpAddress")
     Ok(response)
     }
